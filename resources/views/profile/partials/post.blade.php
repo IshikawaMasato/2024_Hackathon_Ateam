f6f99a41aa2011528ec35e4592014597d8b1954b
akira <a.oono.sys23@morijyobi.sc.jp>
2024-09-18 15:09:28 +0900

    投稿一覧のページの作成(未完成)

---
+++ resources/views/profile/partials/post.blade.php
@@ -0,0 +1,16 @@
+<section>
+    <header class="font-semibold mt-3 ml-11">
+        {{__('投稿一覧')}}
+    </header>
+
+    <form method="" action="">
+        @csrf
+        @method('')
+        
+        
+
+
+    </form>
+
+
+</section>
