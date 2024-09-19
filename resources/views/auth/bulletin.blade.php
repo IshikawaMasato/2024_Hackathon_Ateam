3c29ab0244eb251c207fe63ce65528ca197deb94
Hikaru Matsuba <h.matsuba.sys23@morijyobi.ac.jp>
2024-09-19 09:41:31 +0900

    bulletincssの変更作業

--- resources/views/auth/bulletin.blade.php
+++ resources/views/auth/bulletin.blade.php
@@ -9,44 +9,51 @@
 </head>
 
 <body>
+    <div class="body">
+        <form action="{{ route('reports.store') }}" method="POST" enctype="multipart/form-data">
+            <div class="post-container">
+                <div class="flex">
+                    <div class="flex flex2">
+                        <h1>防災情報投稿</h1>
+                        <img src="{{ asset('img/icon.png') }}" class="icon" alt="">
+                    </div>
+                </div>
+                <div class="profile-section">
+                    <div class="profile-icon"></div>
+                    <div class="profile-info">
+                        <span>アカウント名</span>
+                        <span class="profile">2024/XX/XX</span>
+                    </div>
 
-    <form action="{{ route('reports.store') }}" method="POST" enctype="multipart/form-data">
-        <div class="post-container">
-            <h1>防災情報投稿</h1>
-            <div class="profile-section">
-                <div class="profile-icon"></div>
-                <div class="profile-info">
-                    <span>アカウント名</span>
-                    <span class="profile">2024/XX/XX</span>
                 </div>
                 <div class="title">
                     <input type="text" placeholder="タイトル">
                 </div>
-            </div>
-            <textarea class="post-text"></textarea>
-            <div class="flex">
-                <div class="upload-section">
-                    <input for="imageUpload" type="file" class="upload">
-                    <input type="file" id="imageUpload" style="display: none" />
-                    <div class="image-preview"></div>
+                <textarea class="post-text"></textarea>
+                <div class="flex">
+                    <div class="upload-section">
+                        <input for="imageUpload" type="file" class="upload">
+                        <input type="file" id="imageUpload" style="display: none" />
+                        <div class="image-preview"></div>
+                    </div>
+                    <div class="tag-section">
+                        <select name="category">
+                            <option value="0">全て</option>
+                            @foreach($categorys as $category)
+                            <option value="{{ $category->id }}">{{ $category->tag_name }}</option>
+                            @endforeach
+                        </select>
+                        <button class="tag-button">＋</button>
+                        <button class="tag-button2">－</button>
+                    </div>
                 </div>
-                <div class="tag-section">
-                    <select name="category">
-                        <option value="0">全て</option>
-                        @foreach($categorys as $category)
-                        <option value="{{ $category->id }}">{{ $category->tag_name }}</option>
-                        @endforeach
-                    </select>
-                    <button class="tag-button">＋</button>
-                    <button class="tag-button2">－</button>
+                <div class="button-section">
+                    <button class="back-button">戻る</button>
+                    <button class="submit-button">投稿</button>
                 </div>
             </div>
-            <div class="button-section">
-                <button class="back-button">戻る</button>
-                <button class="submit-button">投稿</button>
-            </div>
-        </div>
-    </form>
+        </form>
+    </div>
 </body>
 
 </html>
